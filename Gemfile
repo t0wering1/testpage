1ba9f80022a4fe6b4e794042fad13e69511340e9
t0wering1 <jon.lin@ucla.edu>
2012-08-22 10:20:13 -0700

    v1.822101912 bare bones

---
+++ Gemfile
@@ -0,0 +1,26 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.8'
+gem 'bootstrap-sass','2.0.0'
+gem 'bcrypt-ruby', '3.0.1'
+group :development do
+	gem 'sqlite3', '1.3.5'
+end
+
+gem 'annotate', '2.5.0', group: :development
+
+group :assets do
+	gem 'sass-rails', '3.2.4'
+	gem 'coffee-rails', '3.2.2'
+	gem 'uglifier', '1.2.3'
+end
+
+gem 'jquery-rails','2.0.2'
+
+group :test do
+	gem 'capybara', '1.1.2'
+end
+
+group :production do
+	gem 'pg', '0.12.2'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testpage::Application.config.secret_token = '47c0a818283613ee040bfb27766ece8001ef88f07c3606c3f4493d50dc06da4d829b45d1c1dfdc9e6f474ac8e2d7d3e5e2415dd8b0f80eac7523f4a3bc4000ff'
